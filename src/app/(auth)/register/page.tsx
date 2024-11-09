1eb2bc65f5b078b1954613088bbd302456fe7dfe
unknown <yohannes.hn16@gmail.com>
2024-11-09 12:03:39 +0700

    Second COmmit

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -4,7 +4,7 @@ import FormRegister from "components/auth/form-register"
 const Register = () => {
   return (
     <div className='p-6 space-y-4'>
-        <h1 className="text-2xl font-bold text-gray-800">Register</h1>
+        <h1 className="text-2xl font-bold text-gray-800">Register Intan Love Jordi</h1>
         <FormRegister/>
     </div>
   )
